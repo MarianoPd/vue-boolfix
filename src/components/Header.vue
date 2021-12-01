3ea3d4b73d173c325db0958cd936b81d3f6dd618
MarianoPd <marianopidone@gmail.com>
2021-12-01 11:06:24 +0100

    first-comit

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <img src="../assets/img/images.png" alt="logo">
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    
+  }
+}
+</script>
+
+
+<style scoped lang="scss">
+@import '../assets/style/generals.scss';
+@import '../assets/style/vars.scss';
+
+header{
+  height: 100px;
+  background-color: black;
+  img{
+    height: 100%;
+    margin-left: 20px;
+  }
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+
+  </main>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+    props:{
+
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../assets/style/generals.scss';
+@import '../assets/style/vars.scss';
+
+main{
+    height: calc(100vh - 100px);
+    background-color: $primary-color;
+}
+</style>
