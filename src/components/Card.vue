750f46569acd0c36c3ce36a61e720d355ff65de5
MarianoPd <marianopidone@gmail.com>
2021-12-02 16:09:29 +0100

    caricamento imagini ok

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,49 +1,60 @@
 <template>
   <div class="my-card col-2">
-    <ul v-if="type === 'movie'">
-        <li><strong>Title:</strong> {{sendResult.title}}</li>
-        <li><strong>Original Title:</strong> {{sendResult.original_title}}</li>
+    <ul >
+        
+        <li><strong>Title:</strong> {{title}}</li>
+        <li><strong>Original Title:</strong> {{originalTitle}}</li>
         <li>
             <img v-if="checkFlagPresence()"  :src="require('../assets/img/'+ sendResult.original_language +'.png')" alt="">
             <strong v-else>{{sendResult.original_language}}</strong>
         </li>
         <li><strong>Vote:</strong> {{sendResult.vote_average}}</li>
+        <img v-if="sendResult.poster_path !== null" :src="imgBaseUrl + sendResult.poster_path" alt="">
         
     </ul>
-    <ul v-if="type === 'serie'">
-        <li><strong>Title:</strong> {{sendResult.name}}</li>
-        <li><strong>Original Title:</strong> {{sendResult.original_name}}</li>
-        <li>
-            <img v-if="checkFlagPresence()"  :src="require('../assets/img/'+ sendResult.original_language +'.png')" alt="">
-            <strong v-else>{{sendResult.original_language}}</strong>
-        </li>
-        <li><strong>Vote:</strong> {{sendResult.vote_average}}</li>
-        
-    </ul>
+    
   </div>
 </template>
 
 <script>
+
 export default {
     name: 'Card',
     data(){
         return{
-
+            imgBaseUrl: 'https://image.tmdb.org/t/p/w342',
+            title:'',
+            originalTitle: '',
+            language: '',
         }
     },
     props:{
         sendResult: Object,
         type: String,
     },
-    computed:{
-        
-    },
+    
     methods:{
         checkFlagPresence(){
             if(this.sendResult.original_language === 'it') return true;
             if(this.sendResult.original_language === 'en') return true;
             return false;
+        },
+        fixProperties(){
+            console.log('watch success');
+            if(this.type === 'movie'){
+                this.title = this.sendResult.title;
+                this.originalTitle = this.sendResult.original_title;
+                this.language = this.sendResult.original_language;
+            }else if(this.type === 'serie'){
+                this.title = this.sendResult.name;
+                this.originalTitle = this.sendResult.original_name;
+                this.language = this.sendResult.original_language;
+            }
+            console.log('watch success');
         }
+    },
+    mounted(){
+        this.fixProperties();
     },
 }    
 </script>
@@ -64,6 +75,9 @@ export default {
                 img{
                     width: 50px;
                 }
+            }
+            img{
+                max-width: 100%;
             }
         }
 
