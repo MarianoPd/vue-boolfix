95df3e1887109d1807940fed0732bd91db34a27d
MarianoPd <marianopidone@gmail.com>
2021-12-03 15:47:35 +0100

    milestone 4 ok

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,29 +1,23 @@
 <template>
-  <div class="my-card col-2">
-    <ul >        
-        <li><strong>Title:</strong> {{title}}</li>
-        <li><strong>Original Title:</strong> {{originalTitle}}</li>
-        <li>
-            <img v-if="checkFlagPresence()"  :src="flag" alt="language">
-            <strong v-else>{{language}}</strong>
-        </li>
-              
-    </ul>
-    <div class="vote">
-            <i v-if="(vote > 0)" class="fas fa-star gold"></i>
-            <i v-else class="fas fa-star gray"></i>
-            <i v-if="(vote > 1)" class="fas fa-star gold"></i>
-            <i v-else class="fas fa-star gray"></i>
-            <i v-if="(vote > 2)" class="fas fa-star gold"></i>
-            <i v-else class="fas fa-star gray"></i>
-            <i v-if="(vote > 3)" class="fas fa-star gold"></i>
-            <i v-else class="fas fa-star gray"></i>
-            <i v-if="(vote > 4)" class="fas fa-star gold"></i>
-            <i v-else class="fas fa-star gray"></i>
-            
-    </div>
-    <h1>{{vote}}</h1>
-    <img v-if="sendResult.poster_path !== null" :src="imgBaseUrl + sendResult.poster_path" alt="">
+  <div class="flip-card col-3 p-0">
+    <div class="flip-card-inner">
+        <div class="flip-card-front">
+            <img  :src="sendResult.poster_path ? imgBaseUrl + sendResult.poster_path : ''" :alt="title">
+        </div>
+        <div class="flip-card-back d-flex flex-column">
+            <div class="info ">
+                <p><strong>Titolo: </strong>{{title}}</p>
+                <p v-if="title !== originalTitle"><strong>Titolo originale: </strong>{{originalTitle}}</p>
+                <img v-if="sendResult.original_language" :src="require('../assets/img/'+ sendResult.original_language +'.png')" :alt="sendResult.original_language">
+                <br>
+                <i v-for="(item, index) in 5" :key="index"  :class="index < vote ? 'fas' : 'far'"  class="fa-star gold"></i>
+            </div>
+            <div v-if="sendResult.overview !== ''" class="overview ">
+                {{sendResult.overview}}
+            </div>
+        </div>
+       
+    </div>    
 
   </div>
 </template>
@@ -35,43 +29,34 @@ export default {
     data(){
         return{
             imgBaseUrl: 'https://image.tmdb.org/t/p/w342',
-            title:'',
-            originalTitle: '',
-            language: '',
-            flag: '',
-            vote: 0,
         }
     },
     props:{
         sendResult: Object,
         type: String,
     },
-    
-    methods:{
-        checkFlagPresence(){
-            if(this.sendResult.original_language === 'it') return true;
-            if(this.sendResult.original_language === 'en') return true;
-            return false;
+
+    computed:{
+        title: function(){
+            if(this.type === 'movie') return this.sendResult.title;
+            if(this.type === 'serie') return this.sendResult.name;
+            return 0;
         },
-        fixProperties(){
-            
-            if(this.type === 'movie'){
-                this.title = this.sendResult.title;
-                this.originalTitle = this.sendResult.original_title;
-            }else if(this.type === 'serie'){
-                this.title = this.sendResult.name;
-                this.originalTitle = this.sendResult.original_name;
-            }
-            this.language = this.sendResult.original_language;
-            this.vote = Math.ceil(this.sendResult.vote_average /2);
-            if(this.checkFlagPresence()){
-                this.flag = require('../assets/img/'+ this.language +'.png');
-            }            
-                        
+        originalTitle: function(){
+            if(this.type === 'movie') return this.sendResult.original_title;
+            if(this.type === 'serie') return this.sendResult.original_name;
+            return 0;
+        },
+        vote: function(){
+            return Math.round(this.sendResult.vote_average /2);
         }
     },
+    
+    methods:{
+        
+    },
     mounted(){
-        this.fixProperties();
+        
         console.log('card mounted');
     },
 }    
@@ -80,33 +65,68 @@ export default {
 <style scoped lang="scss">
 @import '../assets/style/vars.scss';
 @import '../assets/style/generals.scss';
-
-    .my-card{
-        background-color: cadetblue;
-        color: white;
-        border: 5px solid black;
+ 
+    .flip-card {
+        background-color: transparent;
+        height: 490px;
+        border: 1px solid #f1f1f1;
+        perspective: 1000px; 
         
-        ul{
-            list-style: none;
-            li{
-
+        .flip-card-inner {
+            position: relative;
+            width: 100%;
+            height: 100%;
+            transition: transform 0.8s;
+            transform-style: preserve-3d;
+            .flip-card-front, .flip-card-back {
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                -webkit-backface-visibility: hidden; 
+                backface-visibility: hidden;
+                img{
+                    color: white;
+                    font-weight: bolder;
+                    font-size: 20px ;
+                    text-transform: uppercase;
+                }
+            }
+            .flip-card-front {
+                background-color: black;
+                overflow: hidden;
+                img{
+                    max-width: 100%;
+                    text-align: center;
+                }
+            }
+            .flip-card-back {
+                background-color: black;
+                color: white;
+                transform: rotateY(180deg);
+                padding: 15px 15px 0 15px;
+                margin-left: 0;
+                p{
+                    font-size: 20px;
+                }
                 img{
                     width: 50px;
+                    margin-bottom: 20px;
                 }
-            }            
+                i{
+                    color: gold;
+                    font-size: 25px;
+                }
+                
+                .overview{
+                    
+                    overflow-y: auto;
+                }
+            } 
         }
-        img{
-                max-width: 100%;
-        }
-
-        .vote{
-            .gold{
-                color: gold;
-            }
-            .gray{
-                color: gray;
-            }
+        &:hover .flip-card-inner {
+            transform: rotateY(180deg);
         }
 
     }
+
 </style>
